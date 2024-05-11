5579c13b26e62f6eb5e766525e8a6dda86c8b685
NickSim <153093919+NickSimin@users.noreply.github.com>
2024-05-11 18:00:57 +0300

    start

---
+++ game/scripts/tilemap/tile.py
@@ -0,0 +1,6 @@
+import pygame as pg
+from game.scripts.images.image import Image
+
+
+class Tile(Image):
+    pass
---
+++ game/scripts/tilemap/tilemap.py
@@ -0,0 +1,7 @@
+import pygame as pg
+
+from game.scripts.tilemap.tile import Tile
+
+class TileMap:
+    def __init__(self):
+        pass
