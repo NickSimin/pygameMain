ccb997e05b114fdc4c5689ebca50727d600c0338
NickSim <153093919+NickSimin@users.noreply.github.com>
2024-05-19 21:32:03 +0300

    add stone and player animation

--- game/scripts/images/entities/player.py
+++ game/scripts/images/entities/player.py
@@ -14,25 +14,31 @@ class Player(AnimateEntity):
         self.cadres_run = load_images("entities/player/run")
         self.cadres_jump = load_images("entities/player/jump")
         self.cadres_dash = load_images("entities/player/slide")
+        self.cadres_jump_fall = load_images("entities/player/jump_fall")
 
         self.slow_jump = fps // len(self.cadres_run)
         self.slow_run = fps // len(self.cadres_run)
         self.slow_idle = fps // len(self.cadres_idle) * 3
         self.slow_dash = fps // len(self.cadres_dash)
+        self.slow_jump_fall = fps // len(self.cadres_jump_fall)
 
         self.now_cadre_run = 0
         self.now_cadre_idle = 0
         self.now_cadre_jump = 0
         self.now_cadre_dash = 0
+        self.now_cadre_jump_fall = 0
 
+        self.SIZE = (144, 96)
         for cadre in range(len(self.cadres_run)):
-            self.cadres_run[cadre] = pg.transform.scale(self.cadres_run[cadre], (36, 42))
+            self.cadres_run[cadre] = pg.transform.scale(self.cadres_run[cadre], self.SIZE)
         for cadre in range(len(self.cadres_idle)):
-            self.cadres_idle[cadre] = pg.transform.scale(self.cadres_idle[cadre], (36, 42))
+            self.cadres_idle[cadre] = pg.transform.scale(self.cadres_idle[cadre], self.SIZE)
         for cadre in range(len(self.cadres_jump)):
-            self.cadres_jump[cadre] = pg.transform.scale(self.cadres_jump[cadre], (36, 42))
+            self.cadres_jump[cadre] = pg.transform.scale(self.cadres_jump[cadre], self.SIZE)
         for cadre in range(len(self.cadres_dash)):
-            self.cadres_dash[cadre] = pg.transform.scale(self.cadres_dash[cadre], (36, 42))
+            self.cadres_dash[cadre] = pg.transform.scale(self.cadres_dash[cadre], self.SIZE)
+        for cadre in range(len(self.cadres_jump_fall)):
+            self.cadres_jump_fall[cadre] = pg.transform.scale(self.cadres_jump_fall[cadre], self.SIZE)
 
         self.speed = [1, 0]
 
@@ -47,6 +53,8 @@ class Player(AnimateEntity):
         self.is_start_dash = False
         self.is_jump = False
         self.is_start_jump = False
+
+        self.is_fall = False
 
         self.jump_timer = 0
         self.dash_idle_timer = 0
@@ -85,7 +93,7 @@ class Player(AnimateEntity):
         if self.is_jump or self.is_dash:
             return False
         pos = self.position
-        self.position = (pos[0], pos[1] - self.tilemap.tilesize // 4 + 2)
+        self.position = (pos[0] + self.tilemap.tilesize, pos[1] - self.tilemap.tilesize // 2 + 2)
         for cord, tile in self.tilemap.tilemap.items():
             if self.is_collide(tile):
                 self.position = pos
@@ -95,7 +103,10 @@ class Player(AnimateEntity):
 
     def is_stand(self):
         pos = self.position
-        self.position = (pos[0], pos[1] - self.tilemap.tilesize // 4 + 2)
+        if self.is_flip:
+            self.position = (pos[0] - self.tilemap.tilesize, pos[1] - self.tilemap.tilesize // 2 + 2)
+        else:
+            self.position = (pos[0] + self.tilemap.tilesize, pos[1] - self.tilemap.tilesize // 2 + 2)
         for cord, tile in self.tilemap.tilemap.items():
             if self.is_collide(tile):
                 self.position = pos
@@ -116,7 +127,8 @@ class Player(AnimateEntity):
             self.cadres_jump[cadre] = pg.transform.flip(self.cadres_jump[cadre], True, False)
         for cadre in range(len(self.cadres_dash)):
             self.cadres_dash[cadre] = pg.transform.flip(self.cadres_dash[cadre], True, False)
-
+        for cadre in range(len(self.cadres_jump_fall)):
+            self.cadres_jump_fall[cadre] = pg.transform.flip(self.cadres_jump_fall[cadre], True, False)
         self.is_flip = not self.is_flip
 
     def blit(self):
@@ -131,14 +143,14 @@ class Player(AnimateEntity):
                 self.flip()
             elif self.speed[0] > 0 and self.is_flip:
                 self.flip()
-            if not self.is_start_jump and not self.is_start_dash:
+            if not self.is_start_jump and not self.is_start_dash and not self.is_fall:
                 self.screen.blit(self.cadres_run[self.now_cadre_run // self.slow_run % len(self.cadres_run)],
                                  self.position)
                 self.now_cadre_run += 1
             self.now_cadre_idle = 0
             self.move_horisontal()
         else:
-            if not self.is_start_jump and not self.is_start_dash:
+            if not self.is_start_jump and not self.is_start_dash and not self.is_fall:
                 self.screen.blit(self.cadres_idle[self.now_cadre_idle // self.slow_idle % len(self.cadres_idle)],
                                  self.position)
                 self.now_cadre_idle += 1
@@ -147,37 +159,48 @@ class Player(AnimateEntity):
         if self.speed[1] > 0:
             if not self.is_stand():
                 self.tilemap.move((0, self.SPEED))
+                if self.speed[0] < 0 and not self.is_flip:
+                    self.flip()
+                elif self.speed[0] > 0 and self.is_flip:
+                    self.flip()
+                self.screen.blit(self.cadres_jump_fall[self.now_cadre_jump_fall // self.slow_jump_fall \
+                                                       % len(self.cadres_jump_fall)], self.position)
+                self.now_cadre_jump_fall += 1
+                self.is_fall = True
+            else:
+                self.is_fall = False
 
         elif self.speed[1] < 0:
             self.tilemap.move((0, -self.SPEED))
         if self.physics():
             self.speed[1] = 10
 
-        '''print(self.speed[1])'''
-
     def _jump(self):
         self.jump_timer += 1
 
         if self.jump_timer < self.FPS // 3:
-            if self.jump_timer < self.FPS // 4:
-                if self.speed[0] < 0 and not self.is_flip:
-                    self.flip()
-                elif self.speed[0] > 0 and self.is_flip:
-                    self.flip()
-
-                self.screen.blit(self.cadres_jump[self.now_cadre_jump // self.slow_jump % len(self.cadres_jump)],
-                                 self.position)
-                self.now_cadre_jump += 1
-                self.is_start_jump = True
-            else:
-                self.is_start_jump = False
+            if self.speed[0] < 0 and not self.is_flip:
+                self.flip()
+            elif self.speed[0] > 0 and self.is_flip:
+                self.flip()
+            self.screen.blit(self.cadres_jump[self.now_cadre_jump // self.slow_jump % len(self.cadres_jump)],
+                             self.position)
+            self.now_cadre_jump += 1
+            self.is_start_jump = True
             self.speed[1] = -self.FPS / self.jump_timer
 
-        elif self.jump_timer < self.FPS // 3 * 2:
-            self.is_start_jump = False
-            if not self.is_stand():
-                self.speed[1] = self.FPS / self.jump_timer
+        elif self.jump_timer < self.FPS // 3 * 3:
+            self.speed[1] = self.FPS / self.jump_timer
+            if self.speed[0] < 0 and not self.is_flip:
+                self.flip()
+            elif self.speed[0] > 0 and self.is_flip:
+                self.flip()
+            self.screen.blit(self.cadres_jump_fall[self.now_cadre_jump_fall // self.slow_jump_fall \
+                                                   % len(self.cadres_jump_fall)],
+                             self.position)
+            self.now_cadre_jump_fall += 1
         else:
+            self.is_start_jump = False
             self.jump_timer = 0
             self.speed[1] = 0
             self.is_jump = False
--- game/scripts/tilemap/tile.py
+++ game/scripts/tilemap/tile.py
@@ -7,3 +7,9 @@ class Tile(Image):
         super().__init__("tiles/" + path + "/" + str(variant), position, screen)
         self.tilesize = tilesize
         self.image = pg.transform.scale(self.image, (self.tilesize, self.tilesize))
+        self.rect = self.image.get_rect()
+        self.variant = variant
+
+    def blit(self):
+        self.screen.blit(self.image, self.position)
+        #pg.draw.rect(self.screen, (255, 0, 0), (self.position[0], self.position[1], self.tilesize, self.tilesize), 1)
